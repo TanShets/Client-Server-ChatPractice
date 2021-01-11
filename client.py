510116ff7ea5218d638ee8086a99df6d308896b9
TanShets <shettytanish02@gmail.com>
2021-01-11 22:17:13 +0530

    First commit

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 9000
+FORMAT = 'utf-8'
+EXIT_LINES = ["exit", "quit", "close connection"]
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER_IP, PORT)
+
+def send_message(message, client):
+	encoded_message = message.encode(FORMAT)
+	message_length = len(message)
+	head_length = str(message_length).encode(FORMAT)
+	head_length += b' ' * (HEADER - len(head_length))
+	print(client.getsockname()[1])
+	client.send(head_length)
+	client.send(encoded_message)
+
+def receive_message():
+	print(client.recv(2048).decode(FORMAT))
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+new_port = int(client.recv(1024))
+while not new_port:
+	pass
+client.close()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, new_port))
+send_message(input("Enter a username: "), client)
+msg = None
+thread = None
+print("Type a message and press enter to send a message\n")
+while msg == None or msg not in EXIT_LINES:
+	thread = threading.Thread(target = receive_message, args = ())
+	thread.start()
+	msg = input()
+	send_message(msg, client)
+client.close()
---
+++ server.py
@@ -0,0 +1,84 @@
+import socket
+import threading
+import random
+
+class Client:
+	def __init__(self, conn, addr, username, id):
+		self.conn = conn
+		self.addr = addr
+		self.username = username
+		self.id = id
+
+HEADER = 64
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+PORT = 9000
+ADDR = (SERVER_IP, PORT)
+FORMAT = 'utf-8'
+EXIT_LINES = ["exit", "quit", "close connection"]
+MIN, MAX = 1, 1000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+clients = dict()
+
+def start():
+	server.listen()
+	while True:
+		conn, addr = server.accept() #Basically gives the connection with client and address
+		thread = threading.Thread(target = reorient_connection, args = (conn, addr))
+		thread.start()
+		print("Point 1:", threading.activeCount() - 1) #Gives no. of active threads except for main thread
+	conn.close()
+
+def reorient_connection(conn, addr):
+	if(conn):
+		new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		new_socket.bind((SERVER_IP, 0))
+		connection_val = True
+		while connection_val:
+			conn.send(str(new_socket.getsockname()[1]).encode(FORMAT))
+			connection_val = False
+		print(new_socket.getsockname()[1])
+		conn.close()
+		new_socket.listen()
+		while True:
+			new_conn, new_addr = new_socket.accept() #Basically gives the connection with client and address
+			#print("New port here")
+			message_length = new_conn.recv(HEADER).decode(FORMAT)
+			if message_length:
+				message_length = int(message_length)
+				username = new_conn.recv(message_length).decode(FORMAT)
+				new_client = Client(new_conn, new_addr, username, random.randint(MIN, MAX))
+				clients[new_client.id] = new_client
+				thread = threading.Thread(target = handle_client, args = (new_conn, new_addr, username, new_client.id))
+				thread.start()
+			#handle_client(new_conn, new_addr)
+	return
+
+def handle_client(conn, addr, username, id):
+	print("Made it here fam")
+	connection_val = True
+	while connection_val:
+		message_length = conn.recv(HEADER).decode(FORMAT)
+		if message_length:
+			message_length = int(message_length)
+			message = conn.recv(message_length).decode(FORMAT)
+			if message in EXIT_LINES:
+				connection_val = False
+			else:
+				sendAll(username + ": " + message + "\n", id)
+			del message_length
+			del message
+	conn.close()
+	del clients[id]
+	print("Exited")
+	return
+
+def sendAll(message, id):
+	for i in clients.keys():
+		if clients[i].id != id:
+			clients[i].conn.send(message.encode(FORMAT))
+
+print("Initiating")
+start()
